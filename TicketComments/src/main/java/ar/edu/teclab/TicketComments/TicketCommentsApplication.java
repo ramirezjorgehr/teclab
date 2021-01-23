53a0c9f6647ef44b992f4679942f494db71c0b0d
Jorge Ramirez <ramirezjorgehr@gmail.com>
2021-01-23 02:36:23 -0300

    ejercicio finalizado y probado

--- TicketComments/src/main/java/ar/edu/teclab/TicketComments/TicketCommentsApplication.java
+++ TicketComments/src/main/java/ar/edu/teclab/TicketComments/TicketCommentsApplication.java
@@ -4,6 +4,8 @@ import java.net.Authenticator;
 import java.net.PasswordAuthentication;
 import java.net.URI;
 import java.net.http.HttpRequest;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -25,13 +27,13 @@ import org.springframework.web.client.RestTemplate;
 import ar.edu.teclab.dto.Comentario;
 import ar.edu.teclab.dto.Comentarios;
 import java.util.Base64;
+import java.util.Date;
 
 @SpringBootApplication
 public class TicketCommentsApplication {
 
 	private static final Logger log = LoggerFactory.getLogger(TicketCommentsApplication.class);
-	private List<Comentario> comentarioList = new ArrayList<>();
-	private String urlListarComentariosTicket12 = "https://teclab1593636133.zendesk.com/api/v2/tickets/12/comments";
+	private String urlListarComentariosTicket15 = "https://teclab1593636133.zendesk.com/api/v2/tickets/15/comments";
 
 	public static void main(String[] args) {
 		SpringApplication.run(TicketCommentsApplication.class, args);
@@ -42,7 +44,7 @@ public class TicketCommentsApplication {
 		return builder.build();
 	}
 
-	//traigo los comentarios del ticket id = 12
+	// traigo los comentarios del ticket id = 15
 	@Bean
 	public CommandLineRunner listarComentarios(RestTemplate restTemplate) throws Exception {
 		return args -> {
@@ -53,18 +55,22 @@ public class TicketCommentsApplication {
 			HttpHeaders headers = new HttpHeaders();
 			headers.add("Authorization", "Basic " + base64Creds);
 			HttpEntity<String> request = new HttpEntity<String>(headers);
-			ResponseEntity<Object> response = restTemplate.exchange(urlListarComentariosTicket12, HttpMethod.GET,
+			ResponseEntity<Object> response = restTemplate.exchange(urlListarComentariosTicket15, HttpMethod.GET,
 					request, Object.class);
 			log.info(response.getBody().toString());
 		};
 	}
 
-	//inserto un comentario en el ticket id=12
+	// inserto un comentario en el ticket id=15
 	@Bean
 	public CommandLineRunner insertarComentario(RestTemplate restTemplate) throws Exception {
 		return args -> {
-			String requestJson = "{\"ticket\": {\"comment\": {\"body\": \"comentario añadido por Jorge Ramirez\", \"author_id\": 494820284}}}";
-			String url = "https://teclab1593636133.zendesk.com/api/v2/tickets/12";
+			DateFormat formatoHora = new SimpleDateFormat("HH:mm:ss");
+			DateFormat formatoFecha = new SimpleDateFormat("dd/MM/yyyy");
+			String requestJson = "{\"ticket\": {\"comment\": {\"body\": \"comentario añadido por Jorge Ramirez el día "
+					+ formatoFecha.format(new Date()) + " a la hora " + formatoHora.format(new Date())
+					+ "\", \"author_id\": 494820284}}}";
+			String url = "https://teclab1593636133.zendesk.com/api/v2/tickets/15";
 			String plainCreds = "jorge.danni@teclab.edu.ar:Abril2019";
 			byte[] plainCredsBytes = plainCreds.getBytes();
 			byte[] base64CredsBytes = Base64.getEncoder().encode(plainCredsBytes);
@@ -79,7 +85,7 @@ public class TicketCommentsApplication {
 		};
 	}
 
-	//traigo los comentarios del ticket id=12 y los mapeo con la clase comentarios
+	// traigo los comentarios del ticket id=15 y los mapeo con la clase comentarios
 	@Bean
 	public CommandLineRunner traerComentarioList(RestTemplate restTemplate) throws Exception {
 		Authenticator.setDefault(new Authenticator() {
@@ -88,7 +94,7 @@ public class TicketCommentsApplication {
 			}
 		});
 		return args -> {
-			Comentarios comentarioList = restTemplate.getForObject(urlListarComentariosTicket12, Comentarios.class);
+			Comentarios comentarioList = restTemplate.getForObject(urlListarComentariosTicket15, Comentarios.class);
 			log.info(comentarioList.toString());
 		};
 	}
